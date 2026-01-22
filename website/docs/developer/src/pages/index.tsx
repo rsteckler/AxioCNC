0f099bf90db328b37ee382249f1bc2f203d4c477
Ryan Steckler <gotcode@gmail.com>
2026-01-22 13:36:09 -0800

    update devdocs

--- website/docs/developer/src/pages/index.tsx
+++ website/docs/developer/src/pages/index.tsx
@@ -2,6 +2,6 @@ import { Redirect } from '@docusaurus/router';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home(): JSX.Element {
-  const gettingStartedUrl = useBaseUrl('/getting-started');
-  return <Redirect to={gettingStartedUrl} />;
+  const startHereUrl = useBaseUrl('/00-start-here');
+  return <Redirect to={startHereUrl} />;
 }
